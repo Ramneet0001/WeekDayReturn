f8d896fc24d141aea1b3e9ba56060f45c33fb8c6
ramneet singh <ramneet00512@gmail.com>
2019-06-27 22:46:25 +0530

    Initial Commit

---
+++ WeekDayReturn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekDayReturn
+//
+//  Created by STUser on 27/06/19.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
